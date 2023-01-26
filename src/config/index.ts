f9a92e513bd745a3b4411c3319a5008fe3f5e7ac
DreamWorld420 <dreamworld420@protonmail.com>
2023-01-26 18:14:07 +0330

    feat: setup dotenv

---
+++ src/config/index.ts
@@ -0,0 +1,19 @@
+import path from "path";
+import { config } from "dotenv";
+
+config({ path: path.resolve(".", ".env") });
+
+export default {
+	app: {
+		env: process.env?.APP_ENV,
+	},
+	db: {
+		port: process.env?.DB_PORT,
+		user: process.env?.DB_USER,
+		pass: process.env?.DB_PASS,
+		host: process.env?.DB_HOST,
+	},
+	server: {
+		port: process.env?.SERVER_PORT,
+	},
+};
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import loader from "./loaders";
+import config from "./config";
 
 (() => {
 	const app = express();
 
 	loader(app);
 
-	app.listen(3000, () => {
+	app.listen(config.server.port, () => {
 		console.log("server listening");
 	});
 })();
