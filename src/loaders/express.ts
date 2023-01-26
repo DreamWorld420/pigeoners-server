d193ae2b47e74cbecebaa24f2ef596045b1725be
DreamWorld420 <dreamworld420@protonmail.com>
2023-01-26 18:02:34 +0330

    feat: add v1 router

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,5 +1,7 @@
 import express, { Express } from "express";
+import v1Router from "@/api/v1/routes";
 
 export default (app: Express) => {
 	app.use(express.json());
+	app.use("/api/v1", v1Router);
 };
